f87fcf12f0eac7cdefa75dd3d25b242cd91e4279
Meehyun Byeon <bmh2038@naver.com>
2023-05-07 00:29:36 +0900

    2023-05-07-주일

--- models/index.js
+++ models/index.js
@@ -8,43 +8,20 @@ const db = {};
 
 const sequelize = new Sequelize(
   config.database,
-  config.userName,
+  config.username,
   config.password,
   config
 );
 
-// 소문자 Sequelize =>
 db.sequelize = sequelize;
-// 대문자 Sequelize => 시퀄라이즈 객체 => 생성자
-db.Sequelize = Sequelize;
 
 db.User = User;
 db.Comment = Comment;
 
-User.init(sequelize);
-Comment.init(sequelize);
+User.initiate(sequelize);
+Comment.initiate(sequelize);
+
+User.associate(db);
+Comment.associate(db);
 
 module.exports = db;
-
-// const Sequelize = require('sequelize');
-// const User = require('./user');
-// const Comment = require('./comment');
-
-// const env = process.env.NODE_ENV || 'development';
-// const config = require('../config/config')[env];
-// const db = {};
-
-// const sequelize = new Sequelize(config.database, config.username, config.password, config);
-
-// db.sequelize = sequelize;
-
-// db.User = User;
-// db.Comment = Comment;
-
-// User.initiate(sequelize);
-// Comment.initiate(sequelize);
-
-// User.associate(db);
-// Comment.associate(db);
-
-// module.exports = db;
--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const { Comment } = require('../models');
+const express = require("express");
+const { User, Comment } = require("../models");
 
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     const comment = await Comment.create({
       commenter: req.body.id,
@@ -17,14 +17,33 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.route('/:id')
+// router.post("/", async (req, res, next) => {
+//   try {
+//     const user = await User.findOne({ where: { id: req.body.id } });
+//     const comment = await Comment.create({
+//       comment: req.body.comment,
+//     });
+//     const userComment = await user.addComment(comment);
+//     console.log(comment);
+//     res.status(201).json(userComment);
+//   } catch (err) {
+//     console.error(err);
+//     next(err);
+//   }
+// });
+
+router
+  .route("/:id")
   .patch(async (req, res, next) => {
     try {
-      const result = await Comment.update({
-        comment: req.body.comment,
-      }, {
-        where: { id: req.params.id },
-      });
+      const result = await Comment.update(
+        {
+          comment: req.body.comment,
+        },
+        {
+          where: { id: req.params.id },
+        }
+      );
       res.json(result);
     } catch (err) {
       console.error(err);
